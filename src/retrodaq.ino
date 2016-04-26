9c1157a9b4d3fca4ef27eb31478a34faae2e1790
nickbeckwith <nbeckwith2@wisc.edu>
2016-04-26 16:48:21 -0500

    Added distance estimation with error consideration

--- src/retrodaq.ino
+++ src/retrodaq.ino
@@ -1,12 +1,15 @@
 
   float time_elapsed_seconds;
   int num_strips = 0;
-  int inches_normal = 1200;
-  int inches_warning = 2;
+  int inches_normal = 1200;     // distance between early and end strips
+  int inches_warning = 2;       // distance between middle strips
   double inches_per_second = 0; //velocity
+  double caculated_inches_per_second = 880; // variable used for distance estimations
   boolean reflective = false;
-  int count = 1;
   boolean test_in_progress = true;
+  double expected_inches_traveled = 0;
+  double distance_error = 0;
+  double velocity_error = 0;
   
 void setup() {
   // put your setup code here, to run once:
@@ -22,30 +25,29 @@ void loop() {
   unsigned long ending = 0;
   unsigned long temp = 0;
   test_in_progress = true;
+  Serial.println("estimated distance(in) from last strip: ")
   
   while(test_in_progress) {
     
-    
-    
     //Sensor is reading reflective material
     if(digitalRead(8) && reflective) {
-      if(count % 2 == 0) {
+      if((num_strips + 1) % 2 == 0) { // branch is taken on second strip only
         ending = millis();
         temp = starting;
         test_in_progress = false;
       }
       starting = ending;
       num_strips++;
-      count++;
       reflective = false;
-      
-      
     }
 
     //Sensor is not reading reflective material
     if(!digitalRead(8) && test_in_progress) {
       reflective = true;
-      
+      expected_inches_traveled = calculated_inches_per_second * (millis() - starting) / 1000;
+      // prints the inches traveled since last reflective tape
+      Serial.print(expected_inches_traveled);
+      Serial.print(", ");
     }
   }
 
@@ -53,31 +55,41 @@ void loop() {
   
   time_elapsed_seconds = ((float)(ending - temp) / 1000.000);
   
-
   //Start until 1000 feet remaining && end of 500ft remaining until the end
   if(num_strips < 43 || num_strips > 69) {
+    expected_inches_traveled = time_elapsed_seconds * caculated_inches_per_second;
+    distance_error = inches_normal - expected_inches_traveled;
     inches_per_second = (inches_normal * (1/time_elapsed_seconds));
   }
   
   //1000 feet remaining until next 100ft marker
   if(num_strips > 43 && num_strips < 64) {
     //Serial.print("1000 feet remaining");
+    expected_inches_traveled = time_elapsed_seconds * calculated_inches_per_second;
+    distance_error = inches_warning - expected_inches_traveled;
     inches_per_second = (inches_warning *(1/time_elapsed_seconds));
   }
 
   //100ft markers until 500ft marker
   if(num_strips > 64 && num_strips < 68) {
+    expected_inches_traveled = time_elapsed_seconds * calculated_inches_per_second;
+    distance_error = inches_normal - expected_inches_traveled;
     inches_per_second = (inches_normal * (1/time_elapsed_seconds));
   }
 
   //500ft marker until next 100ft marker
   if(num_strips > 68 && num_strips < 69) {
     //Serial.print("500 feet remaining");
+    expected_inches_traveled = time_elapsed_seconds * calculated_inches_per_second;
+    distance_error = inches_warning - expected_inches_traveled;
     inches_per_second = (inches_warning * (1/time_elapsed_seconds));
   }
+  
+  velocity_error = distance_error * (1/time_elapsed_seconds);
+  // accounts for error
+  calculated_inches_per_second = calculated_inches_per_second + velocity_error;
 
   
-
   Serial.print("Start: ");
   Serial.println(starting);
   Serial.print("End: ");
@@ -90,6 +102,4 @@ void loop() {
   Serial.println(inches_per_second);
   Serial.println();
   Serial.println();
- 
-
 }
